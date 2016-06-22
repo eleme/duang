113ef72d46b1f56c7cf3b4125d908daeb372878b
YanagiEiichi <admin@web-tinker.com>
2016-06-22 12:37:40 +0800

    Fix table style with empty actions

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -2,8 +2,9 @@ def((Item, TableCell) => class extends Item {
   get template() { return `<tr></tr>`; }
   init() {
     let { scheme, fields, id } = this;
+    let { actions = [] } = scheme;
     fields = [...fields];
-    if (scheme.actions) fields.push({ type: 'actions', value: scheme.actions, align: 'right' });
+    if (actions.length) fields.push({ type: 'actions', value: scheme.actions, align: 'right' });
     TableCell.cast(fields || [], { scheme, id: this.id }).renderTo(this);
   }
 });
