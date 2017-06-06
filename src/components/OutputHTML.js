4fae22491e0ebd372d442d433262608b952d6e93
YanagiEiichi <admin@web-tinker.com>
2017-06-06 14:20:50 +0800

    Fallbacks args.html to this.value for OutputHTML

--- src/components/OutputHTML.js
+++ src/components/OutputHTML.js
@@ -10,11 +10,15 @@ def(() => class extends Jinkela {
     this.value = this.value;
   }
   render() {
-    this.element.innerHTML = String(this.html).replace(/\{(.*?)\}/g, ($0, key) => {
-      let base = typeof this.value === 'string' ? this : this.value;
-      return key.split('.').reduce((base, name) => Object(base)[name], base);
-    }).replace(/<script>([\s\S]*?)<\/script>/g, ($0, code) => {
-      return new Function(`return (${code})`)();
-    });
+    if (this.html) {
+      this.element.innerHTML = String(this.html).replace(/\{(.*?)\}/g, ($0, key) => {
+        let base = this.value instanceof Object ? this.value : this;
+        return key.split('.').reduce((base, name) => Object(base)[name], base);
+      }).replace(/<script>([\s\S]*?)<\/script>/g, ($0, code) => {
+        return new Function(`return (${code})`)();
+      });
+    } else {
+      this.element.innerHTML = this.value;
+    }
   }
 });
