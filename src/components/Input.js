d95e28919c0289519acb4f4ddeb7edd5fc8b2578
YanagiEiichi <admin@web-tinker.com>
2017-06-06 14:18:55 +0800

    Support hybrid Input/Output

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,8 @@
 def((XPut) => class extends XPut {
   buildComponent() {
     let { component = 'String', args = {}, depot } = this;
-    return req('Input' + component).catch(() => {
+    let [ , hint = 'Input', name ] = String(component).match(/^(Input|Output)?(.*)$/);
+    return req(hint + name).catch(() => {
       throw new Error(`Unknown component "${component}"`);
     }).then(Component => {
       this.result = new Component(args, { depot }).to(this);
--- src/components/Output.js
+++ src/components/Output.js
@@ -3,7 +3,8 @@ def((XPut) => class extends XPut {
     let { component, args = {}, params, query, depot, value } = this;
     void params;
     void query;
-    return req('Output' + component).catch(() => {
+    let [ , hint = 'Output', name ] = String(component).match(/^(Input|Output)?(.*)$/);
+    return req(hint + name).catch(() => {
       throw new Error(`Unknown component "${component}"`);
     }).then(Component => {
       this.result = new Component(args, { value, depot }).to(this);
