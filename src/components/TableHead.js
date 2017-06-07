97c7e281931d072e8c4d01b1b3f2462b47e855b5
YanagiEiichi <admin@web-tinker.com>
2017-06-07 13:24:46 +0800

    Update Constants

--- src/components/TableHead.js
+++ src/components/TableHead.js
@@ -5,7 +5,7 @@ def((TableHeadCell) => class extends Jinkela {
     let { scheme, uParams } = depot;
     let fields = (scheme.fields || []).slice(0);
     if (scheme.actions && scheme.actions.length) {
-      fields.push({ title: depot.getConst('Actions'), nowrap: true });
+      fields.push({ title: depot.getConst('操作'), nowrap: true });
     }
     TableHeadCell.cast(fields).to(this.tr);
     this.element.addEventListener('sort', e => {
