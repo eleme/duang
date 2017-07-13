e93445c5c42f773df9fa5a2d21403d86827e4a3d
YanagiEiichi <admin@web-tinker.com>
2017-07-13 18:02:42 +0800

    Update style for TableCaption

--- src/components/TableCaption.js
+++ src/components/TableCaption.js
@@ -1,3 +1,12 @@
 def((Caption) => class extends Caption {
   get tagName() { return 'caption'; }
+  get styleSheet() {
+    return `
+      :scope {
+        background: #eff2f7;
+        border-bottom: 1px solid #e0e6ed;
+        padding: 0 18px;
+      }
+    `;
+  }
 });
