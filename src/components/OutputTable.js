bf8ff7ef149d73a91c08fb533ba97b0c0be4c969
YanagiEiichi <admin@web-tinker.com>
2017-07-24 14:51:41 +0800

    Fix OutputTable

--- src/components/OutputTable.js
+++ src/components/OutputTable.js
@@ -14,14 +14,25 @@ def((Table) => {
   }
 
   return class extends Jinkela {
+
     get tagName() { return 'div'; }
+
+    get value() { return this.$value; }
+    set value(value = this.defaultValue) {
+      if (this.oTable) this.oTable.data = value;
+      this.$value = value;
+      this.$hasValue = true;
+    }
+
     init() {
       let uParams = new UParams({ module: 'list', key: depot.key });
       let newDepot = depot.fork(uParams);
       let scheme = { fields: this.fields, key: depot.key, module: 'list' };
       Object.defineProperty(newDepot, 'scheme', { value: scheme, configurable: true });
-      new OTable({ depot: newDepot, data: this.value }).to(this);
+      this.oTable = new OTable({ depot: newDepot, data: this.value }).to(this);
+      if (this.$hasValue) this.value = this.$value;
     }
+
   };
 
 });
