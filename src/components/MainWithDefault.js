ef9785fbf37fbe9be5231010f8e1e15688d2b1d9
YanagiEiichi <admin@web-tinker.com>
2017-08-02 12:28:43 +0800

    Fix href for MainWithDefault

--- src/components/MainWithDefault.js
+++ src/components/MainWithDefault.js
@@ -20,17 +20,16 @@ def((Item) => {
         this.element.dataset.char = this.text ? this.text.match(/[^-]*$/g)[0].trimLeft()[0] : 'X';
       }
     }
-    onClick() {
-      let { module = 'list', key, where = {}, params = {} } = this;
+    async onClick() {
+      let { href, target, module = 'list', key, where = {}, params = {} } = this;
       let tasks = [];
       if (this['@where']) tasks.push(api([ this.key, this['@where'] ]).then(result => (where = result)));
       if (this['@params']) tasks.push(api([ this.key, this['@params'] ]).then(result => (params = result)));
-      const done = () => {
-        where = JSON.stringify(where);
-        params = JSON.stringify(params);
-        location.hash = '#' + new UParams({ module, key, where, params });
-      };
-      tasks.length ? Promise.all(tasks).then(done) : done();
+      await Promise.all(tasks);
+      where = JSON.stringify(where);
+      params = JSON.stringify(params);
+      if (href) return open(href, target);
+      return depot.go({ args: { module, key, params, where }, target });
     }
     get styleSheet() {
       let radius = 40;
