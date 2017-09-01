f1aa9c6b19a0476d3f026e5a11df891f3942d518
YanagiEiichi <admin@web-tinker.com>
2017-09-01 15:03:29 +0800

    Fix Shadow

--- src/duang.js
+++ src/duang.js
@@ -55,11 +55,8 @@
 {
   let dependencies = [
     [
-      window.fetch ? null : 'https://github.elemecdn.com/uglifyjs!github/fetch/v0.11.0/fetch.js'
-    ].filter(Boolean),
-    [ // Basic
-      'https://shadow.elemecdn.com/npm/excavator@0.2.1/bundle.min.js,/npm/jinkela@1.2.19/umd.min.js',
-      'https://shadow.elemecdn.com/gh/YanagiEiichi/fceptor@0.2.11/fceptor.min.js,/gh/chjj/marked@v0.3.6/marked.min.js,/gh/YanagiEiichi/uparams@1.4.0/UParams.min.js,/gh/s3u/JSONPath@v0.15.0/lib/jsonpath.min.js',
+      window.fetch ? null : 'https://github.elemecdn.com/uglifyjs!github/fetch/v0.11.0/fetch.js',
+      'https://shadow.elemecdn.com/npm/excavator@0.2.1/bundle.min.js,/npm/jinkela@1.2.21/umd.min.js,/npm/vanilla.js@1.0.0/index.js,/gh/jinkelajs/jinkela-datepicker@1.2.3/datepicker.js,/gh/jinkelajs/jinkela-timepicker@1.0.0/timepicker.js,/gh/jinkelajs/jinkela-dialog@0.1.6/dialog.js,/gh/jinkelajs/jinkela-cascader@1.0.0/index.js,/gh/jinkelajs/jinkela-forest@1.0.2/index.js,/gh/jinkelajs/jinkela-checkbox@1.0.0/index.js,/gh/jinkelajs/jinkela-radio@1.0.0/index.js,/gh/jinkelajs/jinkela-clicktip@1.0.0/index.js,/gh/YanagiEiichi/fceptor@0.2.11/fceptor.min.js,/gh/chjj/marked@v0.3.6/marked.min.js,/gh/s3u/JSONPath@v0.15.0/lib/jsonpath.min.js,/gh/YanagiEiichi/uparams@1.4.0/UParams.min.min.js',
       'utils/api.js',
       'utils/doAction.js',
       'utils/refactor.js',
@@ -68,18 +65,12 @@
       'utils/amdx.js',
       'https://shadow.elemecdn.com/gh/codemirror/CodeMirror@5.19.0/lib/codemirror.min.js'
     ],
-    [
-      'https://shadow.elemecdn.com/gh/requirejs/requirejs@2.2.0/require.min.js',
-      'https://shadow.elemecdn.com/gh/codemirror/CodeMirror@5.19.0/mode/yaml/yaml.min.js,/gh/codemirror/CodeMirror@5.19.0/mode/markdown/markdown.min.js'
-    ],
-    [ // Components
-      'https://shadow.elemecdn.com/gh/jinkelajs/jinkela-datepicker@1.2.3/datepicker.js,/gh/jinkelajs/jinkela-timepicker@1.0.0/timepicker.js,/gh/jinkelajs/jinkela-dialog@0.1.6/dialog.js,/gh/jinkelajs/jinkela-cascader@1.0.0/index.js,/gh/jinkelajs/jinkela-forest@1.0.2/index.js,/gh/jinkelajs/jinkela-checkbox@1.0.0/index.js,/gh/jinkelajs/jinkela-radio@1.0.0/index.js,/gh/jinkelajs/jinkela-clicktip@1.0.0/index.js'
-    ],
-    [ // Entry
-      'utils/depot.js'
-    ]
+    [ 'https://shadow.elemecdn.com/gh/codemirror/CodeMirror@5.19.0/mode/yaml/yaml.min.js,/gh/codemirror/CodeMirror@5.19.0/mode/markdown/markdown.min.js' ],
+    [ 'https://shadow.elemecdn.com/gh/requirejs/requirejs@2.2.0/require.min.js' ],
+    [ 'utils/depot.js' ]
   ];
   const loadScript = src => new Promise(resolve => {
+    if (!src) return resolve();
     let script = document.createElement('script');
     script.setAttribute('src', src);
     script.addEventListener('load', resolve);
