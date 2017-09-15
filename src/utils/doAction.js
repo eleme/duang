7b43872784fb11279fc83e197dfe889211d47945
YanagiEiichi <admin@web-tinker.com>
2017-09-15 19:21:38 +0800

    Fix replace/assign endless wating

--- src/utils/doAction.js
+++ src/utils/doAction.js
@@ -3,8 +3,8 @@ const doAction = (data, depot = window.depot) => {
   args = refactor(args, depot);
   switch (action) {
     case 'confirm': return req('Confirm').then(Confirm => Confirm.popup(args));
-    case 'replace': return new Promise(() => { location.replace(args.href); });
-    case 'assign': return new Promise(() => { location.assign(args.href); });
+    case 'replace': return new Promise(resolve => { location.replace(args.href); setTimeout(resolve, 300); });
+    case 'assign': return new Promise(resolve => { location.assign(args.href); setTimeout(resolve, 300); });
     case 'open': return open(args.href);
     case 'go': return location.replace('#!' + new UParams(args));
     case 'get':
