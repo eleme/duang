9f6eaf71542d2d2bb065d073809f8a3f68275777
YanagiEiichi <admin@web-tinker.com>
2017-09-20 20:09:05 +0800

    Support array value for OutputEnum

--- src/components/OutputEnum.js
+++ src/components/OutputEnum.js
@@ -1,5 +1,18 @@
-def(() => class extends Jinkela {
+def((Output) => class extends Jinkela {
   init() {
-    this.element.innerHTML = this.map[this.value];
+    let { value, map } = this;
+    // 强转 map
+    map = Object(map);
+    if (map instanceof Array) {
+      let temp = {};
+      map.forEach(({ value, text }) => {
+        temp[value] = text;
+      });
+     map = temp;
+    }
+    // 处理所有 value（支持数组）
+    [].concat(value).forEach(value => {
+      Output.createAny(map[value] || '').to(this);
+    });
   }
 });
