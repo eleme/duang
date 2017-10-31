42f0d76275eba3012947ab611ba08495f521bef5
YanagiEiichi <admin@web-tinker.com>
2017-10-31 20:55:11 +0800

    Support sub-component in InputCaption

--- src/components/InputCaption.js
+++ src/components/InputCaption.js
@@ -1,29 +1,34 @@
-def(() => class extends Jinkela {
+def((Output) => class extends Jinkela {
   get template() {
     return `
       <div>
-        <h1>{h1}</h1>
-        <h2>{h2}</h2>
+        <h1><meta ref="h1Ref" /></h1>
+        <h2><meta ref="h2Ref" /></h2>
       </div>
     `;
+  }
+  set h1(what) {
+    this.h1Ref = Output.createAny(what);
+  }
+  set h2(what) {
+    this.h2Ref = Output.createAny(what);
   }
   get styleSheet() {
     return `
       :scope {
         margin-top: 1em;
+        display: flex;
+        align-items: center;
         h1 {
-          display: inline-block;
           font-size: 1.5em;
           margin: 0;
         }
         h2 {
-          display: inline-block;
           font-size: 1em;
           font-weight: normal;
           margin: 0;
           margin-left: 1em;
         }
-        overflow: hidden;
       }
       /* 黑科技 */
       .table > :first-child :scope {
