40a07228bc65f20cf3c387293164213550d8e0ab
YanagiEiichi <admin@web-tinker.com>
2018-05-23 02:12:47 +0800

    Upgrade timepicker

--- src/duang.js
+++ src/duang.js
@@ -76,7 +76,7 @@
     [
       group(
         window.fetch ? null : w`https://github.elemecdn.com/uglifyjs!github/fetch/v0.11.0/fetch.js`,
-        w`https://shadow.elemecdn.com/bundle/npm/excavator@0.2.1/bundle.js,npm/jinkela@1.2.21/umd.min.js,gh/jinkelajs/jinkela-datepicker@1.2.4/datepicker.js,gh/jinkelajs/jinkela-timepicker@1.0.0/timepicker.js,gh/jinkelajs/jinkela-dialog@0.1.6/dialog.js,gh/jinkelajs/jinkela-cascader@1.0.0/index.js,gh/jinkelajs/jinkela-forest@1.0.3/index.js,gh/jinkelajs/jinkela-checkbox@1.0.0/index.js,gh/jinkelajs/jinkela-radio@1.0.0/index.js,gh/jinkelajs/jinkela-clicktip@1.0.0/index.js,gh/chjj/marked@v0.3.6/marked.min.js,gh/YanagiEiichi/uparams@1.4.0/UParams.min.js,npm/stale-while-revalidate@0.1.0/bundle.min.js,gh/s3u/JSONPath@v0.15.0/lib/jsonpath.min.js,npm/vanilla.js@1.0.0/index.js`,
+        w`https://shadow.elemecdn.com/bundle/npm/excavator@0.2.1/bundle.js,npm/jinkela@1.2.21/umd.min.js,gh/jinkelajs/jinkela-datepicker@1.2.4/datepicker.js,npm/jinkela-timepicker@1.1.1/umd.js,gh/jinkelajs/jinkela-dialog@0.1.6/dialog.js,gh/jinkelajs/jinkela-cascader@1.0.0/index.js,gh/jinkelajs/jinkela-forest@1.0.3/index.js,gh/jinkelajs/jinkela-checkbox@1.0.0/index.js,gh/jinkelajs/jinkela-radio@1.0.0/index.js,gh/jinkelajs/jinkela-clicktip@1.0.0/index.js,gh/chjj/marked@v0.3.6/marked.min.js,gh/YanagiEiichi/uparams@1.4.0/UParams.min.js,npm/stale-while-revalidate@0.1.0/bundle.min.js,gh/s3u/JSONPath@v0.15.0/lib/jsonpath.min.js,npm/vanilla.js@1.0.0/index.js`,
         w`utils/api.js`,
         w`utils/doAction.js`,
         w`utils/refactor.js`,
