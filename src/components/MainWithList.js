254a1c9b95d418ba769fed656bc296ba4ad88837
YanagiEiichi <admin@web-tinker.com>
2018-08-13 13:54:51 +0800

    Fix "noWhere"

--- src/components/MainWithList.js
+++ src/components/MainWithList.js
@@ -20,8 +20,8 @@ def((FatalError, Output, ListFlex, ListOperations, ListHeaders, ListFilters, Tab
             <jkl-list-filters depot="{depot}"></jkl-list-filters>
           </jkl-list-flex>
           <jkl-table if="{list}" depot="{depot}" data="{list}" ref="table"></jkl-table>
-          <div class="tip" if="{tip}">{tip}</div>
-          <jkl-fatal-error depot="{depot}" message="{message}" if="{message}"></jkl-fatal-error>
+          <div class="tip" if="{hasTip}"><meta ref="tipContent" /></div>
+          <meta ref="fatal" />
           <meta ref="pagination" />
         </div>
       `;
@@ -40,12 +40,10 @@ def((FatalError, Output, ListFlex, ListOperations, ListHeaders, ListFilters, Tab
       `;
     }
 
-    set count(value) {
-      this.$count = value;
-      this.visible = value === value;
+    beforeParse(params) {
+      this.depot = params.depot || depot;
+      this.isFilterHiddenDefault = this.depot.params.filterState === 'folded';
     }
-
-    get count() { return this.$count; }
 
     filterToggle() {
       this.filterContainer.toggle();
@@ -62,56 +60,61 @@ def((FatalError, Output, ListFlex, ListOperations, ListHeaders, ListFilters, Tab
       return api(resolvedKey + '/count' + '?' + queryParams).catch(() => 0 / 0);
     }
 
-    beforeParse(params) {
-      this.depot = params.depot || depot;
-      let { scheme } = this.depot;
-      void scheme;
-      this.isFilterHiddenDefault = this.depot.params.filterState === 'folded';
+    initData() {
+      let { depot } = this;
+      return Promise.all([
+        this.loadData(),
+        this.loadCount()
+      ]).then(([ list, count ]) => {
+        if (!(list instanceof Array)) throw new Error('返回结果必须是数组');
+        this.list = list;
+        this.count = typeof count === 'number' ? count : count.count;
+        // 如果设置了 pageSize，那么初始化分页控件
+        if ('pageSize' in depot.scheme) this.pagination = new Pagination({ depot, list, count });
+        return list.length;
+      });
     }
 
-    initData() {
-      return Promise.resolve().then(() => {
-        let { depot } = this;
-        let { scheme, where } = depot;
-        let { noWhere, fields = [] } = scheme;
-
-        if (!fields || !fields.length) throw new Error('字段未配置');
-        // 如果设置了 noWhere，那么在 where 为空时将不发起数据加载
-        if (noWhere && Object.keys(where).length === 0) {
-          if (noWhere === true) {
-            this.tip.hide();
-          } else {
-            // noWhere 可以是一个 Output，作为提示用
-            this.tip.text = Output.createAny(noWhere);
-          }
-        } else {
-          // 加载数据
-          return Promise.all([
-            this.loadData(),
-            this.loadCount()
-          ]).then(([ list, count ]) => {
-            if (!(list instanceof Array)) throw new Error('返回结果必须是数组');
-            this.list = list;
-            this.count = typeof count === 'number' ? count : count.count;
-            // 如果设置了 pageSize，那么初始化分页控件
-            if ('pageSize' in depot.scheme) this.pagination = new Pagination({ depot, list, count });
-            return list.length;
-          });
-        }
-      });
+    get tip() { return this.tipContent; }
+    set tip(value) {
+      this.hasTip = !!value;
+      this.tipContent = value;
     }
 
     init() {
       Object.defineProperty(this.depot, 'main', { configurable: true, value: this });
+
+      let { depot } = this;
+      let { scheme, where } = depot;
+      let { noWhere, fields = [] } = scheme;
+
+      // 检查字段完整性
+      if (!fields || !fields.length) return this.errorHandler(new Error('字段未配置'));
+
+      // 如果设置了 noWhere，那么在 where 为空时将不发起数据加载
+      if (noWhere && Object.keys(where).length === 0) {
+        this.tip = noWhere === true ? '请提供查询条件' : Output.createAny(noWhere);
+        return;
+      }
+
+      // 加载数据
       this.tip = '加载中 ···';
       this.promise = this.initData().then(length => {
         this.tip = length ? '' : '查不到匹配条件的数据';
         this.loading = false;
-      }, error => {
-        this.tip = '';
+      }, this.errorHandler);
+    }
+
+    get errorHandler() {
+      let value = error => {
         if (typeof error === 'string') error = { message: '错误信息是一个字符串' };
-        this.message = error.message || '未知错误';
-      });
+        let message = error.message || '未知错误';
+        this.fatal = new FatalError({ depot: this.depot, message });
+        this.tip = '';
+        setTimeout(() => { throw error; });
+      };
+      Object.defineProperty(this, 'errorHandler', { value, configurable: true });
+      return value;
     }
 
   };
